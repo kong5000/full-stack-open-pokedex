03e1c4aa97fce4ca42439a0da33bc559e3797add
k <keith.ong5000@gmail.com>
2022-08-23 15:49:27 -0700

    fix eslint warnings

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001// eslint-disable-line
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-use-before-define
-  console.log('server started on port 5000')
+  console.log('server started on port 5000')// eslint-disable-line
 })
